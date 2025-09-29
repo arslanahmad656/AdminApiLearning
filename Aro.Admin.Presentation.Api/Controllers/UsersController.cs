b2624b65d5a46cf78b24a6ce18534cc7ac158311
Arslan Ahmad <ars.ahm6@gmail.com>
2025-09-29 18:40:12 +0500

    user controller

---
+++ Aro.Admin.Presentation.Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Aro.Admin.Application.Mediator.User.Commands;
+using Aro.Admin.Presentation.Api.DTOs;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MediatorDtos = Aro.Admin.Application.Mediator.User.DTOs;
+
+namespace Aro.Admin.Presentation.Api.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController(IMediator mediator, IMapper mapper) : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserModel model, CancellationToken cancellationToken)
+    {
+        var response = await mediator.Send(new CreateUserCommand(mapper.Map<MediatorDtos.CreateUserRequest>(model)), cancellationToken).ConfigureAwait(false);
+
+        return CreatedAtAction("TODO", response);
+    }
+}
---
+++ Aro.Admin.Presentation.Api/DTOs/CreateUserModel.cs
@@ -0,0 +1,4 @@
+﻿namespace Aro.Admin.Presentation.Api.DTOs;
+
+public record CreateUserModel(string Email, bool IsActive, string Password, string DisplayName, ICollection<string> AssignedRoles);
+
