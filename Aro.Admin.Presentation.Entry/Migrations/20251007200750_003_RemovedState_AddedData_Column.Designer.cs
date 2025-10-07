74b271fb4562ff3ded92dfa4193bb058edfbb489
Adil Farhan <adil.farhan@aro.ie>
2025-10-08 01:13:37 +0500

    Removed the states columns from the audit trail and added the data column.

--- Aro.Admin.Presentation.Entry/Migrations/20251007200750_003_RemovedState_AddedData_Column.Designer.cs
+++ Aro.Admin.Presentation.Entry/Migrations/20251007200750_003_RemovedState_AddedData_Column.Designer.cs
@@ -4,6 +4,7 @@ using Aro.Admin.Infrastructure.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aro.Admin.Presentation.Entry.Migrations
 {
     [DbContext(typeof(AroAdminApiDbContext))]
-    partial class AroAdminApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251007200750_003_RemovedState_AddedData_Column")]
+    partial class _003_RemovedState_AddedData_Column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
