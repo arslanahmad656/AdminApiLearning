cb93c4e37ee0f5f35d8b6bc8b9c409db3a402cfd
Adil Farhan <adil.farhan@aro.ie>
2025-10-13 19:02:46 +0500

    Added the serilogger.

---
+++ Aro.Admin.Application.Services/ILogManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Aro.Admin.Application.Services;
+
+public interface ILogManager
+{
+    void LogInfo(string messageTemplate, params object[] propertyValues);
+    void LogWarn(string messageTemplate, params object[] propertyValues);
+    void LogDebug(string messageTemplate, params object[] propertyValues);
+    void LogError(string messageTemplate, params object[] propertyValues);
+    void LogError(Exception exception, string messageTemplate, params object[] propertyValues);
+}
