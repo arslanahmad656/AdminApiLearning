e1a1874000a38fd23a9a3cf77b985ca1760de151
Arslan Ahmad <ars.ahm6@gmail.com>
2025-10-13 19:02:46 +0500

    Added the serilogger.

---
+++ Aro.Admin.Infrastructure.Services/SeriLogger.cs
@@ -0,0 +1,19 @@
+﻿using Aro.Admin.Application.Services;
+using Serilog;
+
+namespace Aro.Admin.Infrastructure.Services;
+
+public class SeriLogger : ILogManager
+{
+    private readonly ILogger logger = Log.Logger;
+
+    public void LogDebug(string messageTemplate, params object[] propertyValues) => logger.Debug(messageTemplate, propertyValues);
+
+    public void LogError(string messageTemplate, params object[] propertyValues) => logger.Error(messageTemplate, propertyValues);
+
+    public void LogError(Exception exception, string messageTemplate, params object[] propertyValues) => logger.Error(exception, messageTemplate, propertyValues);
+
+    public void LogInfo(string messageTemplate, params object[] propertyValues) => logger.Information(messageTemplate, propertyValues);
+
+    public void LogWarn(string messageTemplate, params object[] propertyValues) => logger.Warning(messageTemplate, propertyValues);
+}
---
+++ Aro.Admin.Presentation.Entry/ServiceInstallers/LoggerInstaller.cs
@@ -0,0 +1,20 @@
+﻿
+using Serilog;
+
+namespace Aro.Admin.Presentation.Entry.ServiceInstallers;
+
+public class LoggerInstaller : IServiceInstaller
+{
+    public void Install(WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+
+        builder.Services.AddSingleton<Application.Services.ILogManager, Infrastructure.Services.SeriLogger>();
+        builder.Services.AddSingleton(typeof(Application.Services.ILogManager<>), typeof(Infrastructure.Services.SeriLogger<>));
+    }
+}
