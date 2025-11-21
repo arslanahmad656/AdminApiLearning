73b1dd789e2bb0decfa6b401fc72cec49f2aeceb
Adil Farhan <adil.farhan@aro.ie>
2025-11-22 02:24:56 +0500

    Settings based azure diagnostics.

--- API/Aro.Common.Infrastructure.Services.Azure/FileManager/Extensions/BlobFileManagerExtensions.cs
+++ API/Aro.Common.Infrastructure.Services.Azure/FileManager/Extensions/BlobFileManagerExtensions.cs
@@ -1,14 +1,23 @@
 ﻿using Aro.Common.Application.Services.FileManager;
+using Aro.Common.Domain.Shared;
+using Aro.Common.Domain.Shared.Exceptions;
+using Aro.Common.Infrastructure.Services.Azure.FileManager.Options;
 using Azure.Core;
 using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Aro.Common.Infrastructure.Services.Azure.FileManager.Extensions;
 
 public static class BlobFileManagerExtensions
 {
-    public static IServiceCollection AddBlobFileManagement(this IServiceCollection services)
+    public static IServiceCollection AddBlobFileManagement(this IServiceCollection services, IConfiguration configuration)
     {
+        var azureSettings = configuration
+                                       .GetSection("AzureSettings")
+                                       .Get<AzureSettings>() 
+                                       ?? throw new AroInvalidOperationException(new ErrorCodes().UNKNOWN_ERROR, $"Could not get the app settings against AzureSettings section.");
+
         services.AddSingleton<TokenCredential>(_ =>
         {
             var options = new DefaultAzureCredentialOptions
@@ -21,7 +30,11 @@ public static class BlobFileManagerExtensions
                 }
             };
 
-            return new DefaultAzureCredential(options);
+            DefaultAzureCredential creds = azureSettings.EnableCredentialLogging
+                ? new (options)
+                : new ();
+
+            return creds;
         });
         services.AddSingleton<IFileManagerFactory, BlobFileManagerFactory>();
         return services;
