8f466cb21224302bb1ece42bb6d2862487d8514b
Adil Farhan <adil.farhan@aro.ie>
2025-11-26 03:16:24 +0500

    Pending migration

--- API/Aro.Admin.Presentation.Entry/Migrations/20251125220831_RemovedTheComment.Designer.cs
+++ API/Aro.Admin.Presentation.Entry/Migrations/20251125220831_RemovedTheComment.Designer.cs
@@ -4,6 +4,7 @@ using Aro.Common.Infrastructure.Repository.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aro.Admin.Presentation.Entry.Migrations
 {
     [DbContext(typeof(AroDbContext))]
-    partial class AroDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251125220831_RemovedTheComment")]
+    partial class RemovedTheComment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
