b1d830a8e5c49e335cdeaae391bcceea4cfb8992
Adil Farhan <adil.farhan@aro.ie>
2025-12-23 03:00:12 +0500

    Countries table

---
+++ API/Aro.Admin.Presentation.Entry/Migrations/20251222215944_CountriesInDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aro.Admin.Presentation.Entry.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountriesInDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    OfficialName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ISO2 = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    PostalCodeRegex = table.Column<string>(type: "nvarchar(max)", maxLength: -1, nullable: false),
+                    PhoneCountryCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    PhoneNumberRegex = table.Column<string>(type: "nvarchar(max)", maxLength: -1, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
