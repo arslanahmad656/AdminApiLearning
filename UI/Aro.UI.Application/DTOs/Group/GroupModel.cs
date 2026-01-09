73c8bbe6100108b728303787bff7b92456be4ce1
Jack Coyle <Jack.coyle@aro.ie>
2026-01-09 15:40:04 +0000

    Group review and save hotfix #2

--- UI/Aro.UI.Application/DTOs/Group/GroupModel.cs
+++ UI/Aro.UI.Application/DTOs/Group/GroupModel.cs
@@ -5,20 +5,20 @@ public class GroupModel
     public Guid Id { get; set; } = Guid.Empty;
 
     // Default
-    public string GroupName { get; set; } = string.Empty;
-    public string? AddressLine1 { get; set; } = string.Empty;
-    public string? AddressLine2 { get; set; } = string.Empty;
-    public string? City { get; set; } = string.Empty;
-    public string? Country { get; set; } = string.Empty;
-    public string? PostalCode { get; set; } = string.Empty;
+    //public string GroupName { get; set; } = string.Empty;
+    //public string? AddressLine1 { get; set; } = string.Empty;
+    //public string? AddressLine2 { get; set; } = string.Empty;
+    //public string? City { get; set; } = string.Empty;
+    //public string? Country { get; set; } = string.Empty;
+    //public string? PostalCode { get; set; } = string.Empty;
 
     // Testing
-    //public string GroupName { get; set; } = "Jack's Group";
-    //public string? AddressLine1 { get; set; } = "Address Line 1";
-    //public string? AddressLine2 { get; set; } = "Address Line 2";
-    //public string? City { get; set; } = "City";
-    //public string? Country { get; set; } = "GB";
-    //public string? PostalCode { get; set; } = "BT7 2FB";
+    public string GroupName { get; set; } = "Jack's Group";
+    public string? AddressLine1 { get; set; } = "Address Line 1";
+    public string? AddressLine2 { get; set; } = "Address Line 2";
+    public string? City { get; set; } = "City";
+    public string? Country { get; set; } = "United Kingdom";
+    public string? PostalCode { get; set; } = "BT7 2FB";
 
     public ImageModel? Logo { get; set; }
     public required PrimaryContactModel PrimaryContact { get; set; }
