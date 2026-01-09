b9f2094e6db6c383b895ba371d622614c9aabe57
Jack Coyle <Jack.coyle@aro.ie>
2026-01-09 15:51:55 +0000

    Revert "Reapply "Group review and save hotfix #2""
    This reverts commit bd69153f1a139d4d16d203ca51e1d3634ce19e53.

--- UI/Aro.UI.Application/DTOs/Group/PrimaryContactModel.cs
+++ UI/Aro.UI.Application/DTOs/Group/PrimaryContactModel.cs
@@ -5,14 +5,14 @@ public class PrimaryContactModel
     public Guid Id { get; set; } = Guid.Empty;
 
     // Default
-    //public string Email { get; set; } = string.Empty;
-    //public string Name { get; set; } = string.Empty;
-    //public string CountryCode { get; set; } = string.Empty;
-    //public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string CountryCode { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
 
     // Testing
-    public string Email { get; set; } = "jack.coyle@aro.ie";
-    public string Name { get; set; } = "Jack Coyle";
-    public string CountryCode { get; set; } = "44";
-    public string PhoneNumber { get; set; } = "750012345";
+    //public string Email { get; set; } = "jack.coyle@aro.ie";
+    //public string Name { get; set; } = "Jack Coyle";
+    //public string CountryCode { get; set; } = "44";
+    //public string PhoneNumber { get; set; } = "750012345";
 }
